72f88fa0b298bfeb7670be9796723767d626c226
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2024-06-04 15:46:25 +0100

    Handle strings with multiple words

--- script.rb
+++ script.rb
@@ -2,14 +2,15 @@ def substrings(string, array_substrings)
   input_string_array = string.downcase.split
 
   array_substrings.reduce(Hash.new(0)) do |hash, substring|
-    if input_string_array.any? {|input_string| input_string.include?(substring)}
-      hash[substring] += 1
+    input_string_array.each do |input_string| 
+      if input_string.include?(substring)
+        hash[substring] += 1
+      end
     end
     hash
   end
 end
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p substrings("below", dictionary)
 
-
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
